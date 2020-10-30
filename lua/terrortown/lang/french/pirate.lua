fa0906cf0b99dfd7dc1fd056078103e5976cce52
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-10-30 22:14:15 +0100

    Create pirate.lua (#9)

---
+++ lua/terrortown/lang/french/pirate.lua
@@ -0,0 +1,20 @@
+local L = LANG.GetLanguageTableReference("french")
+
+L[PIRATE.name] = "Pirate"
+L["info_popup_" .. PIRATE.name] = [[Vous êtes un Pirate! ARRR ! Cherchez quelqu'un pour qui il vaut la peine de se battre - gagnez de l'or et des points.]]
+L["body_found_" .. PIRATE.abbr] = "C'était un Pirate..."
+L["search_role_" .. PIRATE.abbr] = "Cette personne était un Pirate!"
+L["target_" .. PIRATE.name] = "Pirate"
+L["ttt2_desc_" .. PIRATE.name] = [[Le Pirate est un rôle neutre. Il ne se soucie pas vraiment de ce qui est bon et de ce qui est mauvais... 
+Tout ce qui compte, c'est qu'il y ait de l'argent en jeu. Tant qu'une autre personne détient le contrat du Capitaine des Pirates, tous les Pirates font partie de la même équipe qu'elle.]]
+L["hilite_win_" .. TEAM_PIRATE] = "LES PIRATES ONT GAGNÉ"
+L["win_" .. TEAM_PIRATE] = "Les Pirates ont gagné! ARRRR"
+L["ev_win_" .. TEAM_PIRATE] = "Les Pirates ont réclamé leur or!"
+
+L[PIRATE_CAPTAIN.name] = "Capitaine des Pirates"
+L["info_popup_" .. PIRATE_CAPTAIN.name] = [[Vous êtes le Capitaine des pirates! ARRR ! Cherchez quelqu'un pour qui il vaut la peine de se battre - gagnez de l'or et des points.]]
+L["body_found_" .. PIRATE_CAPTAIN.abbr] = "C'était un Capitaine des Pirates..."
+L["search_role_" .. PIRATE_CAPTAIN.abbr] = "Cette personne était un Capitaine des Pirates!"
+L["target_" .. PIRATE_CAPTAIN.name] = "Capitaine des Pirates"
+L["ttt2_desc_" .. PIRATE_CAPTAIN.name] = [[Le Capitaine des  Pirates est un rôle neutre. Il ne se soucie pas vraiment de ce qui est bon et de ce qui est mauvais... 
+Tout ce qui compte, c'est qu'il y ait de l'argent en jeu. Tant qu'une autre personne détient le contrat du Capitaine des Pirates, tous les Pirates font partie de la même équipe qu'elle.]]
