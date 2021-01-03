0192076621a0f63fb2b1a33a622f1b678762db02
Aram <dragolino6@mail.ru>
2021-01-03 19:16:32 +0300

    Localization changes and improvements

--- lua/terrortown/lang/english/pirate.lua
+++ lua/terrortown/lang/english/pirate.lua
@@ -1,20 +1,20 @@
 L = LANG.GetLanguageTableReference("english")
 
 L[PIRATE.name] = "Pirate"
-L["info_popup_" .. PIRATE.name] = [[You ARRR a Pirate! Search someone to fight for - earn gold and points.]]
-L["body_found_" .. PIRATE.abbr] = "This was an Pirate..."
+L["info_popup_" .. PIRATE.name] = [[You ARRRe a Pirate! Find someone to fight for gold and points!]]
+L["body_found_" .. PIRATE.abbr] = "They were a Pirate!"
 L["search_role_" .. PIRATE.abbr] = "This person was an Pirate!"
 L["target_" .. PIRATE.name] = "Pirate"
-L["ttt2_desc_" .. PIRATE.name] = [[The Pirate is a neutral role. He doesn’t really care about what’s good and what’s evil… 
-all that matters is, that there’s money involved. As long as another person owns the Pirate Captain’s contract, all pirates are on the same team as them.]]
+L["ttt2_desc_" .. PIRATE.name] = [[The Pirate is a neutral role. They don’t really care about what’s good and what’s evil…
+but they do care about gold and everything that is involved. As long as someone has the Pirate Captain’s contract, all pirates are on the same team with the person.]]
 L["hilite_win_" .. TEAM_PIRATE] = "THE PIRATES WON"
 L["win_" .. TEAM_PIRATE] = "The Pirates won! ARRRR"
 L["ev_win_" .. TEAM_PIRATE] = "The Pirates have claimed their gold!"
 
 L[PIRATE_CAPTAIN.name] = "Pirate Captain"
-L["info_popup_" .. PIRATE_CAPTAIN.name] = [[You ARRR a Pirate Captain! Search someone to fight for - earn gold and points.]]
-L["body_found_" .. PIRATE_CAPTAIN.abbr] = "This was an Pirate Captain..."
-L["search_role_" .. PIRATE_CAPTAIN.abbr] = "This person was an Pirate Captain!"
+L["info_popup_" .. PIRATE_CAPTAIN.name] = [[You ARRRe a Pirate Captain! Search someone to fight for - earn gold and points.]]
+L["body_found_" .. PIRATE_CAPTAIN.abbr] = "They were a Pirate Captain!"
+L["search_role_" .. PIRATE_CAPTAIN.abbr] = "This person was a Pirate Captain!"
 L["target_" .. PIRATE_CAPTAIN.name] = "Pirate Captain"
-L["ttt2_desc_" .. PIRATE_CAPTAIN.name] = [[The Pirate Captain is a neutral role. He doesn’t really care about what’s good and what’s evil… 
-all that matters is, that there’s money involved. As long as another person owns the Pirate Captain’s contract, all pirates are on the same team as them.]]
+L["ttt2_desc_" .. PIRATE_CAPTAIN.name] = [[The Pirate Captain is a neutral role. They don’t really care about what’s good and what’s evil…
+but they do care about gold and everything that is involved. As long as someone has the Pirate Captain’s contract, all pirates are on the same team with the person.]]
