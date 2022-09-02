e026323158877bd015c03cb3ddaaf8651fabb318
sbzl <92261479+sbzlzh@users.noreply.github.com>
2022-09-03 01:17:35 +0800

    Add simplified Chinese Translation

---
+++ lua/terrortown/lang/zh_hans/pirate.lua
@@ -0,0 +1,23 @@
+L = LANG.GetLanguageTableReference("zh_hans")
+
+L[PIRATE.name] = "海盗"
+L[PIRATE.defaultTeam] = "海盗队伍"
+L["info_popup_" .. PIRATE.name] = [[你是海盗!找个人为之奋斗-赢取金牌和积分.]]
+L["body_found_" .. PIRATE.abbr] = "这是一个海盗..."
+L["search_role_" .. PIRATE.abbr] = "这个人是海盗!"
+L["target_" .. PIRATE.name] = "海盗"
+L["ttt2_desc_" .. PIRATE.name] = [[海盗是一个中立的角色.他们不在乎什么是好什么是坏…
+
+重要的是这里面有钱.只要另一个人拥有海盗船长的合同,所有海盗都是同一个团队的成员.]]
+L["hilite_win_" .. TEAM_PIRATE] = "海盗队伍获胜"
+L["win_" .. TEAM_PIRATE] = "海盗赢了!"
+L["ev_win_" .. TEAM_PIRATE] = "海盗们已认领了他们的黄金!"
+
+L[PIRATE_CAPTAIN.name] = "海盗船长"
+L["info_popup_" .. PIRATE_CAPTAIN.name] = [[你是海盗船长!搜索要为之奋斗的人-赢取金牌和积分.]]
+L["body_found_" .. PIRATE_CAPTAIN.abbr] = "这是一个海盗船长..."
+L["search_role_" .. PIRATE_CAPTAIN.abbr] = "这个人是海盗船长!"
+L["target_" .. PIRATE_CAPTAIN.name] = "海盗船长"
+L["ttt2_desc_" .. PIRATE_CAPTAIN.name] = [[海盗船长是一个中立的角色.他们不在乎什么是好什么是坏…
+
+重要的是这里面有钱.只要另一个人拥有海盗船长的合同,所有海盗都是同一个团队的成员.]]
